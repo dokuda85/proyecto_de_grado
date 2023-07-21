7863108ce93ec82d730af77924a085e07fcbae52
dokuda85 <70708042+dokuda85@users.noreply.github.com>
2023-07-20 21:47:47 -0400

    Update index.php

--- proyecto_grado/index.php
+++ proyecto_grado/index.php
@@ -5,11 +5,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
-    <h1>hola</h1>
+<style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f9f9f9;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
 
-<script src='jquery.min.js'></script>
-<script src='components/funtion.js'></script>
+    .fade-in {
+      opacity: 0;
+      animation: fadeInAnimation 2s ease forwards;
+    }
+
+    @keyframes fadeInAnimation {
+      to {
+        opacity: 1;
+      }
+    }
+  </style>
+<body>
+<div class="fade-in">
+    <h1>Bienvenido a mi sitio web</h1>
+    <p>Redireccionando a otra_pagina.html en 5 segundos...</p>
+  </div>
+<script>
+    // Función para redireccionar a otra_pagina.html después de 5 segundos
+    setTimeout(function() {
+        window.location.href = "page/login/login.php";
+    }, 1000); // 5000 milisegundos = 5 segundos
+</script>
 </body>
 </html>
